62f5d10950e141f88907c33320a39576981f22ef
Hasan Turken <turkenh@gmail.com>
2021-11-30 13:43:16 +0300

    Fix import paths and package names
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- config/sks/config.go
+++ config/sks/config.go
@@ -1,13 +1,12 @@
-package repository
+package sks
 
 import "github.com/crossplane-contrib/terrajet/pkg/config"
 
-// Configure configures individual resources by adding custom ResourceConfigurators.
+// Customize configures individual resources by adding custom ResourceConfigurators.
 func Customize(p *config.Provider) {
     p.AddResourceConfigurator("exoscale_sks_cluster", func(r *config.Resource) {
-
         // we need to override the default group that terrajet generated for
         // this resource, which would be "github"  
-        r.ShortGroup = "sks_cluster"
+        r.ShortGroup = "sks"
     })
 }
