8ec790c71e071bc83c035a70ae1263edba70f25e
Luc Juggery <luc.juggery@gmail.com>
2022-01-04 09:19:22 +0100

    Add ShortGroup to cluster and nodepool

--- config/sks/config.go
+++ config/sks/config.go
@@ -6,13 +6,13 @@ import "github.com/crossplane-contrib/terrajet/pkg/config"
 func Customize(p *config.Provider) {
 	p.AddResourceConfigurator("exoscale_sks_cluster", func(r *config.Resource) {
                 r.Kind = "Cluster"
-		// r.ShortGroup = "sks"
+		r.ShortGroup = "sks"
 		r.ExternalName = config.IdentifierFromProvider
 	})
 
         p.AddResourceConfigurator("exoscale_sks_nodepool", func(r *config.Resource) {
                 r.Kind = "Nodepool"
-                //  r.ShortGroup = "sks"
+                r.ShortGroup = "sks"
                 r.ExternalName = config.IdentifierFromProvider
                 r.References["cluster_id"] = config.Reference{
                         Type:              "Cluster",
