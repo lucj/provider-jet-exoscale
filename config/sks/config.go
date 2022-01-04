5b93185a00816531e0eba519f889b9aa6b274e71
Luc Juggery <luc.juggery@gmail.com>
2022-01-04 15:22:05 +0100

    gofmted

--- config/sks/config.go
+++ config/sks/config.go
@@ -5,27 +5,27 @@ import "github.com/crossplane-contrib/terrajet/pkg/config"
 // Customize configures individual resources by adding custom ResourceConfigurators.
 func Customize(p *config.Provider) {
 	p.AddResourceConfigurator("exoscale_sks_cluster", func(r *config.Resource) {
-                r.Kind = "Cluster"
+		r.Kind = "Cluster"
 		r.ShortGroup = "sks"
 		r.ExternalName = config.IdentifierFromProvider
 	})
 
-        p.AddResourceConfigurator("exoscale_sks_nodepool", func(r *config.Resource) {
-                r.Kind = "Nodepool"
-                r.ShortGroup = "sks"
-                r.ExternalName = config.IdentifierFromProvider
-                r.References["cluster_id"] = config.Reference{
-                        Type:              "Cluster",
-                        RefFieldName:      "ClusterIdRef",
-                }
-                r.References["security_group_ids"] = config.Reference{
-                        //Type:              "SecurityGroup",
-                        Type:              "github.com/lucj/provider-jet-exoscale/apis/securitygroup/v1alpha1.SecurityGroup",
-                        RefFieldName:      "SecurityGroupIdRefs",
-                        SelectorFieldName: "SecurityGroupIdSelector",
-                }
-                r.LateInitializer = config.LateInitializer{
-                        IgnoredFields: []string{"cluster_id", "security_group_ids"},
-                }
-        })
+	p.AddResourceConfigurator("exoscale_sks_nodepool", func(r *config.Resource) {
+		r.Kind = "Nodepool"
+		r.ShortGroup = "sks"
+		r.ExternalName = config.IdentifierFromProvider
+		r.References["cluster_id"] = config.Reference{
+			Type:         "Cluster",
+			RefFieldName: "ClusterIdRef",
+		}
+		r.References["security_group_ids"] = config.Reference{
+			//Type:              "SecurityGroup",
+			Type:              "github.com/lucj/provider-jet-exoscale/apis/securitygroup/v1alpha1.SecurityGroup",
+			RefFieldName:      "SecurityGroupIdRefs",
+			SelectorFieldName: "SecurityGroupIdSelector",
+		}
+		r.LateInitializer = config.LateInitializer{
+			IgnoredFields: []string{"cluster_id", "security_group_ids"},
+		}
+	})
 }
